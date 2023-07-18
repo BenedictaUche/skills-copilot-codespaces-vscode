25499af42bbb672ec12e349c14287aef9d65edcd
Benedicta <benedictauche.onyebuchi@gmail.com>
2023-07-18 06:53:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember()
+{
+    var member = document.getElementById("member").value;
+    var skills = document.getElementById("skills").value;
+    var total = member + skills;
+    return total;
+}
